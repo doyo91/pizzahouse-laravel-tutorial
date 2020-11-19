11c2275f5388f2ad0db88c8f9acb63f9ec3956cc
doyo91 <doyo_91@hotmail.com>
2020-11-19 12:57:48 +0100

    #8- css & image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,21 +17,10 @@
     @endif
 
     <div class="content">
-        <div class="title m-b-md">
-            Pizza House <br/>
+        <img src="assets/images/pizza-house.png" alt="hero image">
+        <h1 class="title m-b-md">
             The North's Best Pizzas
-        </div>
-
-        <div class="links">
-            <a href="https://laravel.com/docs">Docs</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
-            <a href="https://blog.laravel.com">Blog</a>
-            <a href="https://nova.laravel.com">Nova</a>
-            <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://vapor.laravel.com">Vapor</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
-        </div>
+        </h1>
     </div>
 </div>
 @endsection
